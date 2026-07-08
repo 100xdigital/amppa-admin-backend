344380562af25ff400a2b95d084122ce6c60a23c
100xdigital <web100xdigital@gmail.com>
2026-07-08 21:54:05 +0530

    Update blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
 const upload = require('../middlewares/uploadMiddleware');
-const { createBlog, getBlogs,getBlogById, updateBlog, deleteBlog } = require('../controllers/blogController');
+const { createBlog, getBlogs, getBlogById, getBlogBySlug, updateBlog, deleteBlog } = require('../controllers/blogController');
 
+// Public (no login needed)
 router.get('/', getBlogs);
+router.get('/slug/:slug', getBlogBySlug);
 router.get('/:id', getBlogById);
 
+// Admin-only
 router.post('/', auth,upload.single('banner_image'), createBlog);
 router.put('/:id', auth,upload.single('banner_image'), updateBlog);
 router.delete('/:id', auth, deleteBlog);
